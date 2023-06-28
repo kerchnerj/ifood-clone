24b8d5d9fe3db5a3948b6cd6e28df006a5d7579b
kerchnerj <jenifferkerchner12@gmail.com>
2023-06-28 16:07:25 -0300

    alteração

--- src/screens/Busca/index.js
+++ src/screens/Busca/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Buscar() {
+export default function Busca() {
     return (
     <View style={styles.container}>
-        <Text>Buscar</Text>
+        <Text>Busca</Text>
     </View>
     );
 }
